93803b5c58396e4a0ad3b23ef3d36b3ef77c8442
ZhangJian He <shoothzj@gmail.com>
2024-10-09 20:36:19 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/embedded-middleware/embedded-apollo-go/goapollo"
+	_ "github.com/libgox/spring-cloud-go"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
